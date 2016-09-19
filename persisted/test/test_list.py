9c3b3cbb253599947403d71527f5fe90ca353410
Harry Harpham <harry.harpham@evariant.com>
2016-09-19 09:58:41 -0500

    Add init to root package, use unittest library for testing (with Green as runner)

--- persisted/test/test_list.py
+++ persisted/test/test_list.py
@@ -1,82 +1,70 @@
-""" Tests for persisted_list.List """
+""" Tests for persisted.list.List """
 
 import tempfile
-from persisted_list import List
+import unittest
 
-def test_init_new():
-    """ Tests initialization of brand new list.
+from persisted import List
 
-    We return the list created for use in other tests.
+class TestList(unittest.TestCase):
 
-    """
-    return List(tempfile.NamedTemporaryFile().name)
+    def test_init_new(self):
+        " Tests initialization of brand new list. "
+        List(tempfile.NamedTemporaryFile().name)
 
-def test_init_existing():
-    """ Tests initialization of a list from a previous list's backing file.
+    def test_init_existing(self):
+        " Tests initialization of a list from a previous list's backing file. "
+        # First save some stuff so we have something to load.
+        existing_list = List(tempfile.NamedTemporaryFile().name)
+        existing_list.append(1)
+        existing_list.append(2)
+        existing_list.append("to be deleted")
+        existing_list.append("boo")
+        del existing_list[2]
+        newly_loaded_list = List(existing_list._log._backing_file)
+        assert len(newly_loaded_list) == len(existing_list)
+        for index in range(0, len(existing_list)):
+            assert newly_loaded_list[index] == existing_list[index]
 
-    We return the list for use in other tests.
+    def test_init_corrupted(self):
+        " Tests initialization of a list from a badly formatted file. "
+        # Copy the log file then corrupt it by adding a bad line.
+        existing_list = List(tempfile.NamedTemporaryFile().name)
+        existing_list.append(1)
+        existing_list.append(2)
+        existing_list.append("boo")
+        # Sanity check (we're putting the bad data between the 1st and 2nd line).
+        assert len(open(existing_list._log._backing_file).readlines()) > 1
+        copied_backing_file = tempfile.NamedTemporaryFile()
+        bad_line_written = False
+        for line in open(existing_list._log._backing_file).readlines():
+            copied_backing_file.write(line + "\n")
+            if not bad_line_written:
+                copied_backing_file.write("ooga booga I'm corrupted data")
+                bad_line_written = True
+        copied_backing_file.flush()
+        try:
+            List(copied_backing_file.name)
+        except ValueError:
+            # This is what we expected - success.
+            return
+        raise Exception("Init call on corrupted file should have failed")
 
-    """
-    # First save some stuff so we have something to load.
-    existing_list = test_init_new()
-    existing_list.append(1)
-    existing_list.append(2)
-    existing_list.append("to be deleted")
-    existing_list.append("boo")
-    del existing_list[2]
-    newly_loaded_list = List(existing_list._log._backing_file)
-    assert len(newly_loaded_list) == len(existing_list)
-    for index in range(0, len(existing_list)):
-        assert newly_loaded_list[index] == existing_list[index]
-    return newly_loaded_list
+    def test_append_and_get(self):
+        " Tests that elements can be appended and retrieved with get. "
+        pass
 
-def test_init_corrupted():
-    """ Tests initialization of a list from a badly formatted file. """
-    # Copy the log file then corrupt it by adding a bad line.
-    existing_list = test_init_existing()
-    # Sanity check (we're putting the bad data between the 1st and 2nd line).
-    assert len(open(existing_list._log._backing_file).readlines()) > 1
-    copied_backing_file = tempfile.NamedTemporaryFile()
-    bad_line_written = False
-    for line in open(existing_list._log._backing_file).readlines():
-        copied_backing_file.write(line + "\n")
-        if not bad_line_written:
-            copied_backing_file.write("ooga booga I'm corrupted data")
-            bad_line_written = True
-    copied_backing_file.flush()
-    try:
-        List(copied_backing_file.name)
-    except ValueError:
-        # This is what we expected - success.
-        return
-    raise Exception("Init call on corrupted file should have failed")
+    def test_push_and_pop(self):
+        " Tests that elements can be pushed and then popped. "
+        pass
 
-def test_append_and_get():
-    """ Tests that elements can be appended and retrieved with get. """
-    pass
+    def test_index(self):
+        " Tests that the index method returns correct values. "
+        pass
 
-def test_push_and_pop():
-    """ Tests that elements can be pushed and then popped. """
-    pass
+    def test_remove(self):
+        " Tests that the remove method truly removes an element from a list. "
+        pass
 
-def test_index():
-    """ Tests that the index method returns correct values. """
-    pass
-
-def test_remove():
-    """ Tests that the remove method truly removes an element from a list. """
-    pass
-
-def test_size():
-    """ Tests that the size method returns correct values. """
-    pass
-
-test_init_new()
-test_init_existing()
-test_init_corrupted()
-test_append_and_get()
-test_push_and_pop()
-test_index()
-test_remove()
-test_size()
-print "All tests passed!"
+    def test_size(self):
+        " Tests that the size method returns correct values. "
+        pass
--- persisted/test/test_log.py
+++ persisted/test/test_log.py
@@ -4,7 +4,12 @@ import os
 import random
 import string
 import tempfile
-from log import Log
+import unittest
+
+from persisted.log import Log
+
+# Used in tests which don't care about compaction.
+dummy_compaction_callback = lambda: []
 
 
 class DummyOperation():
@@ -49,122 +54,108 @@ class DummyOperation():
         return set_called
 
 
-# Used in tests which don't care about compaction.
-dummy_compaction_callback = lambda: []
+class TestLog(unittest.TestCase):
 
+    def test_init_new(self):
+        " Tests initialization of brand new log. "
+        Log(tempfile.NamedTemporaryFile().name, dummy_compaction_callback)
 
-def test_init_new():
-    """ Tests initialization of brand new log.
+    def test_init_existing(self):
+        " Tests initialization of a log from a previous log's backing file. "
+        # First save some operations so that we have something to load.
+        existing_log = Log(
+            tempfile.NamedTemporaryFile().name, dummy_compaction_callback)
+        for _ in range(10):
+            operation = DummyOperation()
+            existing_log.save_operation(operation.name, *operation.parameters)
+        # Note that this will wipe the log (because the compaction callback returns
+        # an empty list).
+        return Log(existing_log._backing_file, dummy_compaction_callback)
 
-    We return the log created for use in other tests.
+    def test_init_corrupted(self):
+        """ Tests initialization of a log from a badly formatted file. """
+        # Copy the log file then corrupt it by adding a bad line.
+        existing_log = Log(
+            tempfile.NamedTemporaryFile().name, dummy_compaction_callback)
+        for _ in range(10):
+            operation = DummyOperation()
+            existing_log.save_operation(operation.name, *operation.parameters)
+        # Sanity check (we're putting the bad data between the 1st and 2nd line).
+        assert len(open(existing_log._backing_file).readlines()) > 1
+        copied_backing_file = tempfile.NamedTemporaryFile()
+        bad_line_written = False
+        for line in open(existing_log._backing_file).readlines():
+            copied_backing_file.write(line + "\n")
+            if not bad_line_written:
+                copied_backing_file.write("ooga booga I'm corrupted data")
+                bad_line_written = True
+        copied_backing_file.flush()
+        try:
+            Log(copied_backing_file.name, dummy_compaction_callback)
+        except ValueError:
+            # This is what we expected - success.
+            return
+        raise Exception("Init call on corrupted file should have failed")
 
-    """
-    backing_file = tempfile.NamedTemporaryFile()
-    return Log(backing_file.name, dummy_compaction_callback)
+    def test_save_and_replay(self):
+        " Tests that operations are saved in the log and can be replayed. "
+        # Get a fresh log from the test_init_new method.
+        log = Log(
+            tempfile.NamedTemporaryFile().name, dummy_compaction_callback)
+        # Save every operation and simultaneously build up the ops map for the
+        # replay method.
+        dummy_ops = []
+        ops_map = {}
+        for _ in range(10):
+            dummy_op = DummyOperation()
+            dummy_ops.append(dummy_op)
+            log.save_operation(dummy_op.name, *dummy_op.parameters)
+            ops_map[dummy_op.name] = dummy_op.get_function()
+        # Now replay the log and check that every function was called.
+        log.replay(ops_map)
+        for dummy_op in dummy_ops:
+            assert dummy_op.function_called
 
-def test_init_existing():
-    """ Tests initialization of a log from a previous log's backing file.
-
-    We return the log for use in other tests.
-
-    """
-    # First save some operations so that we have something to load.
-    existing_log = test_init_new()
-    for _ in range(10):
+    def test_compact(self):
+        " Tests that compaction reduces log file size when possible. "
         operation = DummyOperation()
-        existing_log.save_operation(operation.name, *operation.parameters)
-    # Note that this will wipe the log (because the compaction callback returns
-    # an empty list).
-    return Log(existing_log._backing_file, dummy_compaction_callback)
+        backing_file = tempfile.NamedTemporaryFile()
+        compaction_callback = lambda: [(operation.name, operation.parameters)]
+        log = Log(backing_file.name, compaction_callback)
+        # Save the same operation many times.
+        for _ in range(100):
+            log.save_operation(operation.name, *operation.parameters)
+        # After compaction, the log should be condensed to one instance of the saved
+        # operation.
+        original_size = os.stat(log._backing_file).st_size
+        log.compact()
+        compacted_size = os.stat(log._backing_file).st_size
+        assert compacted_size < original_size
+        assert compacted_size > 0
 
-def test_init_corrupted():
-    """ Tests initialization of a log from a badly formatted file. """
-    # Copy the log file then corrupt it by adding a bad line.
-    existing_log = test_init_existing()
-    # Sanity check (we're putting the bad data between the 1st and 2nd line).
-    assert len(open(existing_log._backing_file).readlines()) > 1
-    copied_backing_file = tempfile.NamedTemporaryFile()
-    bad_line_written = False
-    for line in open(existing_log._backing_file).readlines():
-        copied_backing_file.write(line + "\n")
-        if not bad_line_written:
-            copied_backing_file.write("ooga booga I'm corrupted data")
-            bad_line_written = True
-    copied_backing_file.flush()
-    try:
-        Log(copied_backing_file.name, dummy_compaction_callback)
-    except ValueError:
-        # This is what we expected - success.
-        return
-    raise Exception("Init call on corrupted file should have failed")
-
-def test_save_and_replay():
-    """ Tests that operations are saved in the log and can be replayed. """
-    # Get a fresh log from the test_init_new method.
-    log = test_init_new()
-    # Save every operation and simultaneously build up the ops map for the
-    # replay method.
-    dummy_ops = []
-    ops_map = {}
-    for _ in range(10):
-        dummy_op = DummyOperation()
-        dummy_ops.append(dummy_op)
-        log.save_operation(dummy_op.name, *dummy_op.parameters)
-        ops_map[dummy_op.name] = dummy_op.get_function()
-    # Now replay the log and check that every function was called.
-    log.replay(ops_map)
-    for dummy_op in dummy_ops:
-        assert dummy_op.function_called
-
-def test_compact():
-    """ Tests that compaction reduces log file size when possible. """
-    operation = DummyOperation()
-    backing_file = tempfile.NamedTemporaryFile()
-    compaction_callback = lambda: [(operation.name, operation.parameters)]
-    log = Log(backing_file.name, compaction_callback)
-    # Save the same operation many times.
-    for _ in range(100):
+    def test_compact_if_necessary(self):
+        " Tests that compaction is run when the threshold is exceeded. "
+        operation = DummyOperation()
+        backing_file = tempfile.NamedTemporaryFile()
+        compaction_callback = lambda: [(operation.name, operation.parameters)]
+        log = Log(backing_file.name, compaction_callback)
+        # Save the same operation many times.
+        for _ in range(100):
+            log.save_operation(operation.name, *operation.parameters)
+        # Set the compaction threshold to less than the current size of the backing
+        # file.
+        original_size = os.stat(log._backing_file).st_size
+        log._compaction_threshold = original_size - 1
+        # Save an operation to trigger compaction.
         log.save_operation(operation.name, *operation.parameters)
-    # After compaction, the log should be condensed to one instance of the saved
-    # operation.
-    original_size = os.stat(log._backing_file).st_size
-    log.compact()
-    compacted_size = os.stat(log._backing_file).st_size
-    assert compacted_size < original_size
-    assert compacted_size > 0
-
-def test_compact_if_necessary():
-    """ Tests that compaction is run when the threshold is exceeded. """
-    operation = DummyOperation()
-    backing_file = tempfile.NamedTemporaryFile()
-    compaction_callback = lambda: [(operation.name, operation.parameters)]
-    log = Log(backing_file.name, compaction_callback)
-    # Save the same operation many times.
-    for _ in range(100):
-        log.save_operation(operation.name, *operation.parameters)
-    # Set the compaction threshold to less than the current size of the backing
-    # file.
-    original_size = os.stat(log._backing_file).st_size
-    log._compaction_threshold = original_size - 1
-    # Save an operation to trigger compaction.
-    log.save_operation(operation.name, *operation.parameters)
-    compacted_size = os.stat(log._backing_file).st_size
-    assert compacted_size < original_size
-    assert compacted_size > 0
-    # Now check that the log never goes over the threshold over the course of
-    # many saves.
-    threshold = log._compaction_threshold
-    for _ in range(1000):
-        log.save_operation(operation.name, *operation.parameters)
-        current_size = os.stat(log._backing_file).st_size
-        assert current_size < threshold
-        assert current_size > 0
-
-
-test_init_new()
-test_init_existing()
-test_init_corrupted()
-test_save_and_replay()
-test_compact()
-test_compact_if_necessary()
-print "All tests passed!"
+        compacted_size = os.stat(log._backing_file).st_size
+        assert compacted_size < original_size
+        assert compacted_size > 0
+        # Now check that the log never goes over the threshold over the course of
+        # many saves.
+        threshold = log._compaction_threshold
+        for _ in range(1000):
+            log.save_operation(operation.name, *operation.parameters)
+            current_size = os.stat(log._backing_file).st_size
+            assert current_size < threshold
+            assert current_size > 0
